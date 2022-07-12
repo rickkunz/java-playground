ae507ecac926337cc0c2a4006874a91281069f33
Rick Kunz <rick.c.kunz@gmail.com>
2022-07-12 15:16:46 -0700

    Roman Numerals

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,24 +1,40 @@
 package com.galvanize;
+import java.util.HashMap;
 
 public class Application {
 
-    public String fizzBuzz (int input) {
-        if (input % 5 == 0 && input % 3 == 0) {
-            return "FizzBuzz";
-        } else if (input % 3 == 0) {
-            return "Fizz";
-        } else if (input % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(input);
+    public int romanNumeralConverter (String input) {
+        int result = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        for (int i = 0; i < input.length(); i++) {
+            int numeralToCheck = i + 1;
+
+            if (numeralToCheck >= input.length()) { //could use OR statement to combine next 'else if'
+                result += map.get(input.charAt(i));
+            } else if (map.get(input.charAt(i)) >= map.get(input.charAt(numeralToCheck))) {
+                result += map.get(input.charAt(i));
+            } else if (map.get(input.charAt(i)) < map.get(input.charAt(numeralToCheck))) {
+                result -= map.get(input.charAt(i));
+            }
         }
+
+        return result;
     }
 
     public static void main(String[] args) {
 
         Application obj = new Application();
 
-        String result = obj.fizzBuzz(Integer.parseInt(args[0]));
+        int result = obj.romanNumeralConverter(args[0]);
 
         System.out.println(result);
     }
